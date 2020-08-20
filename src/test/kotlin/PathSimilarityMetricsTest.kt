483668420f99e04baec9d989db004024c3c42b75
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-08-20 13:02:40 +0300

    Add computeLCSuff test case

--- src/test/kotlin/PathSimilarityMetricsTest.kt
+++ src/test/kotlin/PathSimilarityMetricsTest.kt
@@ -1,4 +1,5 @@
 import model.computeLCP
+import model.computeLCSuff
 import org.junit.Assert
 import org.junit.Test
 
@@ -7,9 +8,15 @@ class PathSimilarityMetricsTest {
     fun `Compute Longest Common Prefix Test`() {
         val testPath1 = "src/com/android/settings/LocationSettings.java"
         val testPath2 = "src/com/android/settings/Utils.java"
-        val LCP = computeLCP(testPath1, testPath2)
-        Assert.assertEquals("LCP of two strings must equal 4", 4, LCP)
+        val LCPLength = computeLCP(testPath1, testPath2)
+        Assert.assertEquals("Longest Common Prefix of two path strings must equal 4", 4, LCPLength)
     }
 
-
+    @Test
+    fun `Compute Longest Common Suffix Test`() {
+        val testPath1 = "src/imports/undo/undo.pro"
+        val testPath2 = "tests/auto/undo/undo.pro"
+        val LCSLength = computeLCSuff(testPath1, testPath2)
+        Assert.assertEquals("Longest Common Suffix of two path strings must equal 2", 2, LCSLength)
+    }
 }
