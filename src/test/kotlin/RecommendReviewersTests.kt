c42c1b379defad736c57d845d785eb4201aa7cf3
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-08-20 15:16:50 +0300

    Add RecommendReviewersTests.kt

---
+++ src/test/kotlin/RecommendReviewersTests.kt
@@ -0,0 +1,8 @@
+import org.junit.Test
+
+class RecommendReviewersTests {
+    @Test
+    fun `Recommend Reviewers with LCP Test`() {
+        TODO("Need to pull test data from Gerrit/JSON first")
+    }
+}
