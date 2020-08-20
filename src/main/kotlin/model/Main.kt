6b7eb8fc7080efd469f1f5ec755cfd092d9c4ee3
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-08-20 15:14:11 +0300

    Add LCP reviewer recommendation to Main.kt

--- src/main/kotlin/model/Main.kt
+++ src/main/kotlin/model/Main.kt
@@ -5,12 +5,37 @@ const val outputFile = "output"
 fun main() {
     println("Starting main.kt")
     val reviewsData = getReviewsData()
-    val reviewers = recommendReviewers(reviewsData)
+    val newReview = reviewsData.last()
+    val reviewers = recommendReviewers(reviewsData.toMutableList(), newReview)
 }
 
-fun recommendReviewers(data: Collection<Review>): Collection<Reviewer> {
+fun recommendReviewers(reviewHistory: MutableList<Review>, newReview: Review): Collection<Reviewer> {
     val reviewers = mutableListOf<Reviewer>()
+    reviewHistory.sortBy { it.submitDate }
+    val reviewersLCP = getReviewersWithLCP(reviewHistory, newReview)
+    return reviewers
+}
 
+fun getReviewersWithLCP(reviewHistory: MutableList<Review>, newReview: Review): Collection<Reviewer> {
+    val reviewers = mutableListOf<Reviewer>()
+    for (pastReview in reviewHistory) {
+        val pastReviewFiles = pastReview.files
+        val newReviewFiles = newReview.files
+        var pastReviewScore = 0f
+        for (newReviewFile in newReviewFiles) {
+            for (pastReviewFile in pastReviewFiles) {
+                pastReviewScore += computeLCPostfixScore(newReviewFile, pastReviewFile)
+            }
+        }
+        pastReviewScore /= (pastReviewFiles.size * newReviewFiles.size)
+        for (pastReviewer in pastReview.getReviewers()) {
+            if (reviewers.any { it.userID == pastReviewer }) {
+                reviewers.find { it.userID == pastReviewer }!!.score += pastReviewScore
+            } else {
+                reviewers.add(Reviewer(pastReviewer, pastReviewScore))
+            }
+        }
+    }
     return reviewers
 }
 
@@ -18,3 +43,4 @@ fun getReviewsData(): Collection<Review> {
     val reviews = mutableListOf<Review>()
     return reviews
 }
+
